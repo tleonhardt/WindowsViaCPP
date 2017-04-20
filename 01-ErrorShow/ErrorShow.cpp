3c42991d8f5cd91fc9675afdacb5754fa88de8bb
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-04-19 21:52:39 -0400

    Format change

--- 01-ErrorShow/ErrorShow.cpp
+++ 01-ErrorShow/ErrorShow.cpp
@@ -20,89 +20,89 @@ const TCHAR g_szAppName[] = TEXT("Error Show");
 ///////////////////////////////////////////////////////////////////////////////
 
 
-BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) 
+BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 {
 
-   chSETDLGICONS(hwnd, IDI_ERRORSHOW);
+    chSETDLGICONS(hwnd, IDI_ERRORSHOW);
 
-   // Don't accept error codes more than 5 digits long
-   Edit_LimitText(GetDlgItem(hwnd, IDC_ERRORCODE), 5);
+    // Don't accept error codes more than 5 digits long
+    Edit_LimitText(GetDlgItem(hwnd, IDC_ERRORCODE), 5);
 
-   // Look up the command-line passed error number
-   SendMessage(hwnd, ESM_POKECODEANDLOOKUP, lParam, 0);
-   return(TRUE);
+    // Look up the command-line passed error number
+    SendMessage(hwnd, ESM_POKECODEANDLOOKUP, lParam, 0);
+    return(TRUE);
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) 
+void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 {
 
-   switch (id) 
-   {
+    switch (id)
+    {
 
-   case IDCANCEL:
-      EndDialog(hwnd, id);
-      break;
+    case IDCANCEL:
+        EndDialog(hwnd, id);
+        break;
 
-   case IDC_ALWAYSONTOP:
-      SetWindowPos(hwnd, IsDlgButtonChecked(hwnd, IDC_ALWAYSONTOP) 
-         ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-      break;
+    case IDC_ALWAYSONTOP:
+        SetWindowPos(hwnd, IsDlgButtonChecked(hwnd, IDC_ALWAYSONTOP)
+            ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+        break;
 
-   case IDC_ERRORCODE: 
-      EnableWindow(GetDlgItem(hwnd, IDOK), Edit_GetTextLength(hwndCtl) > 0);
-      break;
+    case IDC_ERRORCODE:
+        EnableWindow(GetDlgItem(hwnd, IDOK), Edit_GetTextLength(hwndCtl) > 0);
+        break;
 
-   case IDOK:
-      // Get the error code
-      DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
+    case IDOK:
+        // Get the error code
+        DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
 
-	  HLOCAL hlocal = NULL;   // Buffer that gets the error message string
+        HLOCAL hlocal = NULL;   // Buffer that gets the error message string
 
-      // Use the default system locale since we look for Windows messages.
-      // Note: this MAKELANGID combination has 0 as value
-      DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
+        // Use the default system locale since we look for Windows messages.
+        // Note: this MAKELANGID combination has 0 as value
+        DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
 
-      // Get the error code's textual description
-      DWORD fOk = FormatMessage(	// Formats a message string. The function requires a message definition as input.
-		  FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, // dwFlags [in] -The formatting options
-          NULL,				// lpSource [in, optional] - The location of the message definition
-		  dwError,			// dwMessageId [in] - The message identifier for the requested message
-		  systemLocale,		// dwLanguageId [in] - The language identifier for the requested message
-          (PTSTR)&hlocal,	// lpBuffer [out] - A pointer to a buffer that receives the null-terminated string that specifies the formatted message
-		  0,				// nSize [in] - If the FORMAT_MESSAGE_ALLOCATE_BUFFER flag is not set, this parameter specifies the size of the output buffer, in TCHARs.
-		  NULL				// Arguments [in, optional] - An array of values that are used as insert values in the formatted message. A %1 indicates the first value
-	  );	// If the function succeeds, the return value is the number of TCHARs stored in the output buffer, excluding the terminating null character.
+        // Get the error code's textual description
+        DWORD fOk = FormatMessage(	// Formats a message string. The function requires a message definition as input.
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, // dwFlags [in] -The formatting options
+            NULL,				// lpSource [in, optional] - The location of the message definition
+            dwError,			// dwMessageId [in] - The message identifier for the requested message
+            systemLocale,		// dwLanguageId [in] - The language identifier for the requested message
+            (PTSTR)&hlocal,	// lpBuffer [out] - A pointer to a buffer that receives the null-terminated string that specifies the formatted message
+            0,				// nSize [in] - If the FORMAT_MESSAGE_ALLOCATE_BUFFER flag is not set, this parameter specifies the size of the output buffer, in TCHARs.
+            NULL				// Arguments [in, optional] - An array of values that are used as insert values in the formatted message. A %1 indicates the first value
+        );	// If the function succeeds, the return value is the number of TCHARs stored in the output buffer, excluding the terminating null character.
 
-      if (!fOk) 
-	  {
-         // Is it a network-related error?
-         HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, DONT_RESOLVE_DLL_REFERENCES);
+        if (!fOk)
+        {
+            // Is it a network-related error?
+            HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, DONT_RESOLVE_DLL_REFERENCES);
 
-         if (hDll != NULL) 
-		 {
-            fOk = FormatMessage(
-               FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-               hDll, dwError, systemLocale, (PTSTR) &hlocal, 0, NULL);
-            FreeLibrary(hDll);
-         }
-      }
+            if (hDll != NULL)
+            {
+                // Try to look up the message code in the NetMsg.dll module
+                fOk = FormatMessage(FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                    hDll, dwError, systemLocale, (PTSTR)&hlocal, 0, NULL);
+                FreeLibrary(hDll);
+            }
+        }
 
-      if (fOk && (hlocal != NULL)) 
-	  {
-         SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR) LocalLock(hlocal));
-         LocalFree(hlocal);
-      } 
-	  else 
-	  {
-         SetDlgItemText(hwnd, IDC_ERRORTEXT, TEXT("No text found for this error number."));
-      }
+        if (fOk && (hlocal != NULL))
+        {
+            SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR)LocalLock(hlocal));
+            LocalFree(hlocal);
+        }
+        else
+        {
+            SetDlgItemText(hwnd, IDC_ERRORTEXT, TEXT("No text found for this error number."));
+        }
 
-      break;
-   }
+        break;
+    }
 }
 
 
@@ -111,19 +111,19 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 
 INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
-   switch (uMsg) {
-      chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
-      chHANDLE_DLGMSG(hwnd, WM_COMMAND,    Dlg_OnCommand);
+    switch (uMsg) {
+        chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
+        chHANDLE_DLGMSG(hwnd, WM_COMMAND, Dlg_OnCommand);
 
-   case ESM_POKECODEANDLOOKUP:
-      SetDlgItemInt(hwnd, IDC_ERRORCODE, (UINT) wParam, FALSE);
-      FORWARD_WM_COMMAND(hwnd, IDOK, GetDlgItem(hwnd, IDOK), BN_CLICKED, 
-         PostMessage);
-      SetForegroundWindow(hwnd);
-      break;
-   }
+    case ESM_POKECODEANDLOOKUP:
+        SetDlgItemInt(hwnd, IDC_ERRORCODE, (UINT)wParam, FALSE);
+        FORWARD_WM_COMMAND(hwnd, IDOK, GetDlgItem(hwnd, IDOK), BN_CLICKED,
+            PostMessage);
+        SetForegroundWindow(hwnd);
+        break;
+    }
 
-   return(FALSE);
+    return(FALSE);
 }
 
 
@@ -132,15 +132,16 @@ INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int) {
 
-   HWND hwnd = FindWindow(TEXT("#32770"), TEXT("Error Show"));
-   if (IsWindow(hwnd)) {
-      // An instance is already running, activate it and send it the new #
-      SendMessage(hwnd, ESM_POKECODEANDLOOKUP, _ttoi(pszCmdLine), 0);
-   } else {
-      DialogBoxParam(hinstExe, MAKEINTRESOURCE(IDD_ERRORSHOW), 
-         NULL, Dlg_Proc, _ttoi(pszCmdLine));
-   }
-   return(0);
+    HWND hwnd = FindWindow(TEXT("#32770"), TEXT("Error Show"));
+    if (IsWindow(hwnd)) {
+        // An instance is already running, activate it and send it the new #
+        SendMessage(hwnd, ESM_POKECODEANDLOOKUP, _ttoi(pszCmdLine), 0);
+    }
+    else {
+        DialogBoxParam(hinstExe, MAKEINTRESOURCE(IDD_ERRORSHOW),
+            NULL, Dlg_Proc, _ttoi(pszCmdLine));
+    }
+    return(0);
 }
 
 
