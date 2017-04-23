0af1102cb0206a4759c00e81f038e21fd513a1b6
Todd Leonhardt <todd.leonhardt@gmail.com>
2017-04-23 11:31:44 -0400

    Added a coupele "HelloWorld" basic projects, one GUI, the other Console

---
+++ HelloWorldConsole/HelloWorldConsole.cpp
@@ -0,0 +1,15 @@
+// HelloWorldConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#pragma warning(push, 3)
+#include <Windows.h>
+#pragma warning(pop) 
+
+
+int main()
+{
+    _tprintf(TEXT("HelloWorld!\n"));
+    return 0;
+}
+
